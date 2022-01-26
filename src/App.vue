0c32f924ae64f78959565be0ea13fe924394f3a8
Aokiti <yuukiyuuki223@gmail.com>
2022-01-26 14:01:58 +0900

    ユーザー画面を実装

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <router-link to="/">Home </router-link>
-    <router-link to="/screen">Screen</router-link>
+    <router-link to="/screen"> Screen</router-link>
     <router-view />
   </div>
 </template>
@@ -11,5 +11,8 @@
 </script>
 
 <style>
-
+@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@500;900&display=swap');
+#app {
+  font-family: 'Noto Sans JP', sans-serif;
+}
 </style>
--- src/routes/User.vue
+++ src/routes/User.vue
@@ -1,13 +1,91 @@
 <template>
-  <h1>Routing Test Page</h1>
+  <div id="app">
+    <div class="like-button">
+      <div class="button"></div>
+      <div id="lottie" @click="animation"></div>
+    </div>
+    <div class="title">
+      <h1>発表に「いいね」を送る</h1>
+      <span>※一回の発表につき100回まで可能です</span>
+    </div>
+  </div>
 </template>
 
 <script>
+import lottie from "lottie-web";
+
+let animation;
+
 export default {
-  name: "User"
+  name: "Screen",
+  mounted() {
+    animation = lottie.loadAnimation({
+      container: document.querySelector('#lottie'),
+      renderer: 'svg',
+      loop: false,
+      autoplay: false,
+      path: 'https://assets8.lottiefiles.com/packages/lf20_9wcp0umd.json'
+    });
+  },
+  methods: {
+    animation() {
+      animation.playSegments([4, 60], true);
+    }
+  }
 }
 </script>
 
 <style scoped>
+#app {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translateY(-50%) translateX(-50%);
+  overflow: hidden;
+}
 
+.title {
+  text-align: center;
+  width: 80vw;
+  margin-bottom: 32px;
+}
+
+.title h1 {
+  margin-bottom: 8px;
+}
+
+.like-button {
+  position: relative;
+  width: 60vw;
+  height: 60vw;
+  max-width: 320px;
+  max-height: 320px;
+  margin: 24px auto;
+}
+
+.button {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translateY(-50%) translateX(-50%);
+  width: 90%;
+  height: 90%;
+  margin-top: -4px;
+  border-radius: 50%;
+  box-shadow: 0 8px 18px 0 rgba(45, 54, 65, 0.2);
+}
+
+#lottie {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translateY(-50%) translateX(-50%);
+  width: 100%;
+}
+
+@media screen and (max-width: 480px) {
+  .title h1 {
+    font-size: 28px;
+  }
+}
 </style>
