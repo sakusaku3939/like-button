58fa0aa029d2f035b631ec64ab056f1ca905db40
Aokiti <yuukiyuuki223@gmail.com>
2022-02-06 19:22:02 +0900

    発表者を切り替える 画面を追加

---
+++ src/routes/admin/Switch.vue
@@ -0,0 +1,117 @@
+<template>
+  <div id="app">
+    <h1>発表者を切り替える</h1>
+
+    <ul class="list-group">
+      <li v-for="(element) in list" :key="element.title">
+        <i class="fas fa-play play" v-if="element.id === current.id"></i>
+        <div class="play-dummy" v-else></div>
+        <div class="image"></div>
+        <span class="title">{{ element.title }} </span>
+        <div class="heart-counter">
+          <i class="fas fa-heart"></i>
+          <span v-if="element.id === current.id">{{ current.heartCounter }}</span>
+          <span v-else>{{ element.heartCounter }}</span>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+
+let current = {id: 0, heartCounter: 30};
+
+export default {
+  data() {
+    return {
+      list: [
+        {id: 0, title: "presenter1", heartCounter: 0},
+        {id: 1, title: "presenter2", heartCounter: 0},
+        {id: 2, title: "presenter3", heartCounter: 0}
+      ],
+      current,
+    };
+  },
+}
+</script>
+
+<style scoped>
+h1 {
+  text-align: center;
+  margin-top: 64px;
+}
+
+.list-group {
+  margin: 80px 10% 0;
+  padding: 0;
+}
+
+.list-group li {
+  display: flex;
+  align-items: center;
+  list-style: none;
+  padding: 24px 16px;
+}
+
+.list-group li:hover {
+  background-color: #F5F5F5;
+}
+
+.list-group li + li {
+  border-top: 1px solid rgba(0, 0, 0, 0.2);
+}
+
+i.play {
+  color: #212121;
+  font-size: 20px;
+}
+
+.play-dummy {
+  margin-left: 18px;
+}
+
+.heart-counter {
+  display: flex;
+  flex-direction: column;
+  color: rgb(255, 76, 76);
+  margin-left: auto;
+}
+
+.heart-counter i {
+  font-size: 20px;
+}
+
+.heart-counter span {
+  text-align: center;
+  font-size: 14px;
+}
+
+.image {
+  width: 160px;
+  height: 90px;
+  border: 1px solid rgba(0, 0, 0, 0.3);
+  margin: 0 24px;
+}
+
+.title {
+  font-size: 16px;
+}
+
+@media screen and (max-width: 1000px) {
+  .list-group {
+    margin: 80px 56px 0;
+  }
+
+  .image {
+    width: 80px;
+    height: 45px;
+  }
+}
+
+@media screen and (max-width: 600px) {
+  .list-group {
+    margin: 80px 16px 0;
+  }
+}
+</style>
