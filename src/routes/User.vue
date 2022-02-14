bd017ce9358dbc586d8590275f32722fef1fe064
sakusaku3939 <yuukiyuuki223@gmail.com>
2022-02-14 18:04:29 +0900

    コメント送信フォームのデザイン追加

--- src/routes/User.vue
+++ src/routes/User.vue
@@ -1,12 +1,18 @@
 <template>
   <div id="app">
-    <div class="like-button">
-      <div class="button" @click="click_like_button"></div>
-      <div id="lottie"></div>
+    <div class="center">
+      <div class="like-button">
+        <div class="button" @click="click_like_button"></div>
+        <div id="lottie"></div>
+      </div>
+      <div class="title">
+        <h1>発表に「いいね」を送る</h1>
+        <span>※一回の発表につき100回まで可能です</span>
+      </div>
     </div>
-    <div class="title">
-      <h1>発表に「いいね」を送る</h1>
-      <span>※一回の発表につき100回まで可能です</span>
+    <div class="comment">
+      <input type="text" class="input-comment" placeholder="匿名でコメントを送る">
+      <i class="fas fa-paper-plane"></i>
     </div>
   </div>
 </template>
@@ -39,11 +45,14 @@ export default {
 
 <style scoped>
 #app {
+  overflow: hidden;
+}
+
+.center {
   position: absolute;
   top: 50%;
   left: 50%;
   transform: translateY(-50%) translateX(-50%);
-  overflow: hidden;
 }
 
 .title {
@@ -84,6 +93,32 @@ export default {
   left: 50%;
   transform: translateY(-50%) translateX(-50%);
   pointer-events: none;
+}
+
+.comment {
+  position: absolute;
+  display: flex;
+  width: 90%;
+  bottom: 16px;
+  left: 50%;
+  transform: translateX(-50%);
+}
+
+.input-comment {
+  border: none;
+  outline: none;
+  font-size: 16px;
+  text-indent: 8px;
+  width: 100%;
+  padding: 4px 0;
+  border-bottom: 1px solid rgba(0, 0, 0, .42);
+}
+
+.comment i {
+  margin: auto 8px;
+  font-size: 20px;
+  color: rgba(0, 0, 0, .54);
+  cursor: pointer;
 }
 
 @media screen and (max-width: 480px) {
