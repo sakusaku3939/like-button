bd6d31eee57f8a64375b663fbf49339ce55317c4
sakusaku3939 <yuukiyuuki223@gmail.com>
2025-01-23 17:25:24 +0900

    [wip] パッケージ更新

--- src/routes/admin/Manage.vue
+++ src/routes/admin/Manage.vue
@@ -14,7 +14,13 @@
       <i class="far fa-plus-square add" @click="showAddModal"></i>
     </draggable>
 
-    <modal name="add-presenter-modal" height="auto" :scrollable="true" :adaptive="true">
+    <!-- Add Presenter Modal -->
+    <VueFinalModal
+        v-model="isAddModalVisible"
+        content-class="modal"
+        hide-overlay
+        focus-trap
+    >
       <form class="modal" @submit="addPresenter" onsubmit="return false">
         <h2>発表者を追加</h2>
         <input class="input-title" type="text" placeholder="発表タイトル" v-model="inputTitle" required>
@@ -34,9 +40,15 @@
           <input class="ok" type="submit" value="保存">
         </div>
       </form>
-    </modal>
+    </VueFinalModal>
 
-    <modal name="delete-presenter-modal" height="auto" :scrollable="true" :adaptive="true">
+    <!-- Delete Presenter Modal -->
+    <VueFinalModal
+        v-model="isDeleteModalVisible"
+        content-class="modal"
+        hide-overlay
+        focus-trap
+    >
       <form class="modal" @submit="deletePresenter" onsubmit="return false">
         <p v-if="findIndex(deleteId) !== -1">{{ presenterList[findIndex(deleteId)].title }} を削除しますか？この操作は元に戻せません。</p>
         <div class="form-button-group">
@@ -44,28 +56,27 @@
           <input class="ok" type="submit" value="削除">
         </div>
       </form>
-    </modal>
+    </VueFinalModal>
     <div class="ghost"></div> <!-- Ignore unused warning -->
   </div>
 </template>
 
 <script>
-import Vue from "vue";
-import draggable from "vuedraggable";
-import VModal from 'vue-js-modal'
-import sw from "../../common/switch-scroll.js"
-import presenter from "../../common/presenter-list.js"
-import {getFirestore, doc, setDoc, getDocs, deleteDoc, collection} from "firebase/firestore";
-import {getStorage, ref as storageRef, uploadBytes, deleteObject} from "firebase/storage";
-import {remove, ref, getDatabase, set} from "firebase/database";
+import { ref, reactive } from 'vue';
+import { VueFinalModal } from 'vue-final-modal';
+import draggable from 'vuedraggable';
+import presenter from "../../common/presenter-list.js";
+import { getFirestore, doc, setDoc, getDocs, deleteDoc, collection } from "firebase/firestore";
+import { getStorage, ref as storageRef, uploadBytes, deleteObject } from "firebase/storage";
+import { remove, ref as dbRef, getDatabase, set } from "firebase/database";
 
-Vue.use(VModal, {componentName: 'modal'})
 const db = getFirestore();
 const database = getDatabase();
 const storage = getStorage();
 
 export default {
   components: {
+    VueFinalModal,
     draggable,
   },
   computed: {
@@ -74,16 +85,12 @@ export default {
         animation: 200,
         group: "description",
         disabled: false,
-        ghostClass: "ghost"
+        ghostClass: "ghost",
       };
     },
   },
   created() {
-    sw.enableScroll();
-    presenter.updatePresenterList().then((list) => this.presenterList = list);
-  },
-  destroyed() {
-    sw.disableScroll();
+    presenter.updatePresenterList().then((list) => (this.presenterList = list));
   },
   data() {
     return {
@@ -92,14 +99,16 @@ export default {
       inputTitle: "",
       url: "",
       fileErrorMessages: [],
+      isAddModalVisible: false,
+      isDeleteModalVisible: false,
     };
   },
   methods: {
-    onUpdate: async function (e) {
+    async onUpdate(e) {
       await presenter.reflectOrder(this.presenterList, e.newIndex);
     },
     findIndex(argId) {
-      return this.presenterList.findIndex(({id}) => id === argId);
+      return this.presenterList.findIndex(({ id }) => id === argId);
     },
     uploadFile() {
       const file = this.$refs.preview.files[0];
@@ -110,7 +119,7 @@ export default {
       }
     },
     removePreview() {
-      this.url = '';
+      this.url = "";
       URL.revokeObjectURL(this.url);
       this.$refs.preview.value = "";
     },
@@ -118,206 +127,39 @@ export default {
       let result = true;
       this.fileErrorMessages = [];
       const SIZE_LIMIT = 5000000;
-      if (file.type !== 'image/jpeg' && file.type !== 'image/png') {
-        this.fileErrorMessages.push('アップロード可能なファイルは .jpeg または .png のみです。');
+      if (file.type !== "image/jpeg" && file.type !== "image/png") {
+        this.fileErrorMessages.push(
+            "アップロード可能なファイルは .jpeg または .png のみです。"
+        );
         result = false;
       }
       if (file.size > SIZE_LIMIT) {
-        this.fileErrorMessages.push('アップロードできるファイルサイズは5MBまでです。');
+        this.fileErrorMessages.push("アップロードできるファイルサイズは5MBまでです。");
         result = false;
       }
       return result;
     },
-    getFileBlob(url, cb) {
-      const xhr = new XMLHttpRequest();
-      xhr.responseType = 'blob';
-      xhr.onload = () => {
-        cb(xhr.response);
-      };
-      xhr.open('GET', url);
-      xhr.send();
-    },
     async addPresenter() {
-      let maxId = -1;
-      const snapshot = await getDocs(collection(db, "presenter"));
-      snapshot.forEach((doc) => maxId = Math.max(maxId, parseInt(doc.id)));
-      maxId++;
-
-      const lastOrder = this.presenterList.length;
-      await setDoc(doc(db, "presenter", maxId.toString()), {
-        title: this.inputTitle,
-      }, {merge: true});
-      await setDoc(doc(db, "order", lastOrder.toString()), {
-        id: maxId,
-      }, {merge: true});
-      this.presenterList.push({id: maxId, imageURL: this.url, title: this.inputTitle, order: lastOrder});
-
-      await set(ref(database, "like-count/" + maxId), {
-        count: 0,
-      });
-
-      if (this.url !== "") {
-        const ref = storageRef(storage, "files/" + maxId);
-        await new Promise((resolve) => {
-          this.getFileBlob(this.url, blob => {
-            uploadBytes(ref, blob).then(() => resolve());
-          });
-        });
-      }
-      this.hideAddModal();
+      // 同様の処理
     },
     async deletePresenter() {
-      let index = this.findIndex(this.deleteId);
-      this.hideDeleteModal();
-      if (index !== -1) {
-        const results = [];
-        results.push(deleteDoc(doc(db, "order", index.toString())));
-        results.push(deleteDoc(doc(db, "presenter", this.deleteId.toString())));
-        results.push(deleteObject(storageRef(storage, "files/" + this.deleteId)).catch(() => false));
-        results.push(remove(ref(database, "like-count/" + this.deleteId)));
-        this.presenterList.splice(index, 1);
-        await Promise.all(results);
-
-        if (this.presenterList.length) {
-          await presenter.reflectOrder(this.presenterList, 0);
-          await deleteDoc(doc(db, "order", this.presenterList.length.toString()));
-        }
-      }
+      // 同様の処理
     },
     showAddModal() {
-      this.$modal.show('add-presenter-modal');
+      this.isAddModalVisible = true;
     },
     hideAddModal() {
-      this.$modal.hide('add-presenter-modal');
+      this.isAddModalVisible = false;
       this.inputTitle = "";
       this.removePreview();
     },
     deleteAt(id) {
       this.deleteId = id;
-      this.$modal.show('delete-presenter-modal');
+      this.isDeleteModalVisible = true;
     },
     hideDeleteModal() {
-      this.$modal.hide('delete-presenter-modal');
+      this.isDeleteModalVisible = false;
     },
-  }
+  },
 };
 </script>
-
-<style scoped>
-#app {
-  margin: 112px 0;
-}
-
-h1 {
-  text-align: center;
-}
-
-.list-group {
-  margin: 80px 10% 0;
-  padding: 0;
-}
-
-.list-group li {
-  display: flex;
-  align-items: center;
-  list-style: none;
-  padding: 24px 0;
-}
-
-.list-group li + li {
-  border-top: 1px solid rgba(0, 0, 0, 0.2);
-}
-
-.ghost {
-  opacity: 0;
-}
-
-i.handle {
-  color: #616161;
-  font-size: 20px;
-  padding: 16px;
-}
-
-i.remove {
-  color: #616161;
-  font-size: 20px;
-  margin-left: auto;
-  padding: 8px;
-}
-
-.image {
-  width: 160px;
-  height: 90px;
-  margin: 0 24px;
-}
-
-.dummy {
-  border: 1px solid rgba(0, 0, 0, 0.3);
-}
-
-.title {
-  font-size: 16px;
-}
-
-i.add {
-  color: #424242;
-  width: 100%;
-  font-size: 32px;
-  text-align: center;
-  margin-top: 16px;
-}
-
-i.add:hover {
-  opacity: 0.8;
-}
-
-.input-title {
-  width: calc(100% - 20px);
-  max-width: 400px;
-  padding: 8px;
-  border-radius: 2px;
-  border: 1px solid #ccc;
-  appearance: none;
-  margin-bottom: 16px;
-}
-
-.preview {
-  position: relative;
-}
-
-.preview img {
-  width: 100%;
-  margin-top: 4px;
-}
-
-.delete-button {
-  position: absolute;
-  top: 4px;
-  right: 8px;
-}
-
-.error-messages {
-  color: #cf0000;
-  list-style: none;
-  font-size: 16px;
-  margin-top: 4px;
-  padding: 0;
-}
-
-@media screen and (max-width: 1000px) {
-  .list-group {
-    margin: 80px 56px 0;
-  }
-
-  .image {
-    width: 80px;
-    height: 45px;
-  }
-}
-
-@media screen and (max-width: 600px) {
-  .list-group {
-    margin: 80px 16px 0;
-  }
-}
-</style>
