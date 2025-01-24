06101a17ec0e17a8b41bb57de4c999f15d513c1c
sakusaku3939 <yuukiyuuki223@gmail.com>
2025-01-25 00:48:55 +0900

    Firebase環境を更新

--- src/config/firebase-config.js
+++ src/config/firebase-config.js
@@ -1,8 +1,9 @@
 export default {
-    apiKey: "AIzaSyAmYyZ1uJm-OsvJWhKLE0QGfOhcT0WLOZ4",
+    apiKey: "AIzaSyDIzcGVYyBOLbcfJGAeth986TPnGM3vDwc",
     authDomain: "yamabuki-like-button.firebaseapp.com",
+    databaseURL: "https://yamabuki-like-button-default-rtdb.firebaseio.com",
     projectId: "yamabuki-like-button",
     storageBucket: "yamabuki-like-button.appspot.com",
     messagingSenderId: "1054782475089",
-    appId: "1:1054782475089:web:27acc7512e3ac1099e2170"
+    appId: "1:1054782475089:web:b67892d3c3183c549e2170"
 }
