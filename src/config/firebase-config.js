2a829443b8f41405af523d6b26a22f2524503ec7
sakusaku3939 <yuukiyuuki223@gmail.com>
2025-01-25 12:31:25 +0900

    Delete config

--- src/config/firebase-config.js
+++
@@ -1,9 +0,0 @@
-export default {
-    apiKey: "AIzaSyDIzcGVYyBOLbcfJGAeth986TPnGM3vDwc",
-    authDomain: "yamabuki-like-button.firebaseapp.com",
-    databaseURL: "https://yamabuki-like-button-default-rtdb.firebaseio.com",
-    projectId: "yamabuki-like-button",
-    storageBucket: "yamabuki-like-button.appspot.com",
-    messagingSenderId: "1054782475089",
-    appId: "1:1054782475089:web:b67892d3c3183c549e2170"
-}
